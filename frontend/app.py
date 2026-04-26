24280f99a30f001b2fbc1eb45e14bd62277e9d21
Shashwat <shashwattripathi1503@gmail.com>
2026-04-27 02:42:28 +0530

    minor fixes

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
+import os
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(page_title="ContextShift", page_icon="🧠", layout="centered")
 
